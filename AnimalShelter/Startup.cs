22d45bc3f261e2d343246442e1287a7b7e95ab0d
Furculesteanu Bianca <48888793+Bia103@users.noreply.github.com>
2021-01-11 22:23:51 +0200

    Add files via upload

---
+++ AnimalShelter/Startup.cs
@@ -0,0 +1,67 @@
+﻿using AnimalShelter.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnimalShelter.Startup))]
+namespace AnimalShelter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            CreateAdminAndUserRoles();
+        }
+        private void CreateAdminAndUserRoles()
+        {
+            var ctx = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(ctx));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(ctx));
+
+            // adaugam rolurile pe care le poate avea un utilizator
+            // din cadrul aplicatiei
+            if (!roleManager.RoleExists("Admin"))
+            {
+                // adaugam rolul de administrator
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                // se adauga utilizatorul administrator
+                var user = new ApplicationUser();
+                user.UserName = "admin@admin.com";
+                user.Email = "admin@admin.com";
+
+                var adminCreated = userManager.Create(user, "Admin2020!");
+                if (adminCreated.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Admin");
+                }
+
+
+            }
+            if (!roleManager.RoleExists("Voluntar"))
+            {
+                // adaugati rolul specific aplicatiei voastre
+                var role = new IdentityRole();
+                role.Name = "Voluntar";
+                roleManager.Create(role);
+                // se adauga utilizatorul
+                var user = new ApplicationUser();
+                // user.UserName = "your_user_email";
+                // user.Email = "your_user_email";
+                //var user = new ApplicationUser();
+                user.UserName = "voluntar@voluntar.com";
+                user.Email = "voluntar@voluntar.com";
+
+                var adminCreated = userManager.Create(user, "Voluntar2020!");
+                if (adminCreated.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Voluntar");
+                }
+            }
+        }
+    }
+}
